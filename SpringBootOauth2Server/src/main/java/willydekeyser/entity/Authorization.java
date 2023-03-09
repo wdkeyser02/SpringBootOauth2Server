d3d573936367faec0c0a71482844a2d94a27837b
willydekeyser <ma074172@skynet.be>
2023-03-09 15:32:03 +0100

    Update SecurityConfig

--- SpringBootOauth2Server/src/main/java/willydekeyser/entity/Authorization.java
+++ SpringBootOauth2Server/src/main/java/willydekeyser/entity/Authorization.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,40 +26,45 @@ public class Authorization {
 	private String authorizationGrantType;
 	@Column(length = 1000)
 	private String authorizedScopes;
+	@Lob
 	@Column(length = 4000)
 	private String attributes;
 	@Column(length = 500)
 	private String state;
-
+	@Lob
 	@Column(length = 4000)
 	private String authorizationCodeValue;
 	private Instant authorizationCodeIssuedAt;
 	private Instant authorizationCodeExpiresAt;
 	private String authorizationCodeMetadata;
-
+	@Lob
 	@Column(length = 4000)
 	private String accessTokenValue;
 	private Instant accessTokenIssuedAt;
 	private Instant accessTokenExpiresAt;
+	@Lob
 	@Column(length = 2000)
 	private String accessTokenMetadata;
 	private String accessTokenType;
 	@Column(length = 1000)
 	private String accessTokenScopes;
-
+	@Lob
 	@Column(length = 4000)
 	private String refreshTokenValue;
 	private Instant refreshTokenIssuedAt;
 	private Instant refreshTokenExpiresAt;
+	@Lob
 	@Column(length = 2000)
 	private String refreshTokenMetadata;
-
+	@Lob
 	@Column(length = 4000)
 	private String oidcIdTokenValue;
 	private Instant oidcIdTokenIssuedAt;
 	private Instant oidcIdTokenExpiresAt;
+	@Lob
 	@Column(length = 2000)
 	private String oidcIdTokenMetadata;
+	@Lob
 	@Column(length = 2000)
 	private String oidcIdTokenClaims;
 }
